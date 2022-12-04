c3690375dc9eced07b3614d118f9a3dacfe6838c
vinoddalavai <dsvinod.1990@protonmail.com>
2022-12-04 12:44:02 -0500

    Refactor valid_parentheses and min_stack

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,22 +1,39 @@
+"""
+Approach: Stacks
+Time Complexity: O(N)
+Space Complexity: O(N)
+"""
+
 class ValidParentheses:
-    BRACKETS = {
-        ']': '[',
+    # global hash map that maps closing brackets with their matching opening brackets
+    BRACKET_MAP = {
         ')': '(',
+        ']': '[',
         '}': '{'
     }
 
-    def is_valid(self, s: str) -> bool:
+    def _is_valid(self, s: str) -> bool:
+        # initialize an empty stack
         stack = []
-        for char in s:
-            if ((char in self.BRACKETS.keys() and not stack) or
-                    (char in self.BRACKETS.keys() and stack and self.BRACKETS[char] != stack.pop())):
+        # iterate through every bracket of the input string
+        for bracket in s:
+            # two conditions for invalid parentheses:
+            # 1. if stack is empty and string begins with a closing bracket
+            # 2. if stack is not empty and the next bracket in the string is a closing bracket and the top element in
+            # the stack is its corresponding opening bracket
+            if ((bracket in self.BRACKET_MAP.keys() and not stack) or
+                    (stack and bracket in self.BRACKET_MAP.keys() and self.BRACKET_MAP[bracket] != stack.pop())):
                 return False
-            elif char in self.BRACKETS.values():
-                stack.append(char)
+            # if next bracket is an opening bracket then add it to the stack
+            elif bracket in self.BRACKET_MAP.values():
+                stack.append(bracket)
+        # if stack is empty, then all opening brackets have been closed in the right order and that it is a valid input
         return stack == []
 
+    def process(self, input_string: str) -> None:
+        print(f"\nOutput >> {self._is_valid(input_string)}\n")
 
-string1, string2, string3 = '}{', '[()]{}', '()]]'
-print("Validating parentheses for " + string1 + " : " + str(ValidParentheses().is_valid(string1)))
-print("Validating parentheses for " + string2 + " : " + str(ValidParentheses().is_valid(string2)))
-print("Validating parentheses for " + string3 + " : " + str(ValidParentheses().is_valid(string3)))
+
+if __name__ == '__main__':
+    input_str = input("Enter the parentheses string: ")
+    ValidParentheses().process(input_str)
